b186286a32f4f4de8523805424a10cd22125a0c4
Doniben <doniben@esperanto.co>
2020-01-10 00:55:18 +0000

    [FEATURE] function that reads a text file (UTF8) and prints it to stdout + slash

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 # Stting up the web servers to deploy.
+
 sudo apt_get update
 sudo apt-get -y install nginx
 sudo service nginx start
@@ -9,6 +10,6 @@ sudo mkdir -p /data/web_static/releases/test/
 echo "Holberton" | sudo tee -a /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown ubuntu:ubuntu -R /data/
-sudo sed -i '48a\\\n\tlocation hbnb_static/ {\n\t\talias /data/web_static/current;}\n' /etc/nginx/sites-available/default
+sudo sed -i '48a\\n\tlocation hbnb_static/ {\n\t\talias /data/web_static/current;}\n' /etc/nginx/sites-available/default
 sudo service nginx restart
 exit 0
