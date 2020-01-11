8669bda2cb664fadcb74967f067cdbbe2ef77d0c
Doniben <doniben@esperanto.co>
2020-01-11 02:07:50 +0000

    [FEATURE] generates a .tgz archive from the contents of the web_static folder + change in variables

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -17,23 +17,25 @@ env.user = "ubuntu"
 # env.ssh_config_path = '~/.ssh/ssh_config'
 # env.key_filename = '~/.ssh/holberton'
 
+
 def do_deploy(archive_path):
     """  distributes an archive to your web servers """
     if not os.path.exists(archive_path):
         return False
     try:
+        path = "/data/web_static/releases/"
         file_name = os.path.basename(archive_path)
         put('file_name', '/tmp/{}'.format(file_name))
         spaces = file_name.replace('.', ' ')
         without_ext = spaces.split(' ')
-        without_ext1 = with_ext[-2]
-        run('mkdir -p /data/web_static/releases/{}'.format(without_ext1))
-        run('tar -xzf /tmp/{} -C /data/web_static/releases/{}/'.format(file_name, without_ext1))
+        ext_out = with_ext[-2]
+        run('mkdir -p /data/web_static/releases/{}'.format(ext_out))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_name, path, ext_out))
         run('rm /tmp/{}'.format(file_name))
-        run('mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/'.format(without_ext1, without_ext1))
-        run('rm -rf /data/web_static/releases/{}/web_static'.format(without_ext1))
+        run('mv {}{}/web_static/* {}{}/'.format(path, ext_out, path, ext_out))
+        run('rm -rf {}{}/web_static'.format(path, without_ext1))
         run('rm -rf /data/web_static/current')
-        run('ln -s /data/web_static/releases/{}/ /data/web_static/current'.format(without_ext1))
+        run('ln -s {}{}/ /data/web_static/current'.format(path, ext_out))
         return True
     except:
         return False
