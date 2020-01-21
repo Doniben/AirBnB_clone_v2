58903bda3b09534fbe4b43cdd171b42bed2e69ac
Doniben <doniben@esperanto.co>
2020-01-21 02:11:03 +0000

    [FEATURE] script that starts a Flask web application

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" script that starts a Flask web application """
+
+from flask import Flask
+
+app = Flask("__main__")
+
+
+@app.route('/')
+def home():
+    return "Hello HBNB!"
+
+@app.route('/hbnb')
+def hbnb():
+    return "HBNB"
+
+@app.route('/c/<text>')
+def replace_text(text):
+    return text.replace("_", " ")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=False)
