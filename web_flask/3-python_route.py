467f5cf97f65554d8a3366f4a86b0a652a3fd70e
Doniben <doniben@esperanto.co>
2020-01-21 20:41:43 +0000

    [FEATURE] script that starts a Flask web application with /, /hbnb, change in c-text and python-text

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -10,18 +10,21 @@ app = Flask("__main__")
 def home():
     return "Hello HBNB!"
 
+
 @app.route('/hbnb')
 def hbnb():
     return "HBNB"
 
+
 @app.route('/c/<text>')
 def change_underscore(text):
-    return text.replace("_", " ")
+    return "C " + text.replace("_", " ")
 
-@app.route('/python')
-@app.route('/python/(<text>)')
-def py_func():
-    return "Python" + text.replace("_", " ")
+
+@app.route('/python/')
+@app.route('/python/<text>')
+def py_func(text="is cool"):
+    return "Python " + text.replace("_", " ")
 
 if __name__ == "__main__":
-    return (host="0.0.0.0", port=5000, debug=False)
+    app.run(host="0.0.0.0", port=5000, debug=False)
