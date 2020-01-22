4a7e1a93d11a9893f394c8223511abafb905e747
Doniben <doniben@esperanto.co>
2020-01-22 23:24:22 +0000

    [FEATURE] Script that starts a Flask web application with /, /hbnb, /c/<text>

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -11,37 +11,44 @@ app = Flask(__name__)
 
 @app.route('/')
 def route():
+    """ home route """
     return "Hello HBNB!"
 
 
 @app.route('/hbnb')
 def hbnb():
+    """ hbnb route """
     return "HBNB"
 
 
 @app.route('/c/<text>')
 def changed_text(text):
+    """ changed text """
     return "C " + text.replace("_", " ")
 
 
 @app.route('/python/')
 @app.route('/python/<text>')
 def py_function(text="in cool"):
+    """ python function """
     return "Python " + text.replace("_", " ")
 
 
 @app.route('/number/<int:n>')
 def number_function(n):
+    """ number function """
     return "%d is a number" % n
 
 
 @app.route('/number_template/<int:n>')
 def template_function(n):
+    """ template funtion """
     return render_template('5-number.html', n=n)
 
 
-@app.route('/number_odd_or_even/<int:n>')
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
 def odd_or_even(n):
+    """ odd or even function """
     return render_template('6-number_odd_or_even.html', n=n)
 
 
