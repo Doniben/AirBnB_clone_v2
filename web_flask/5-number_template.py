b56c8507d2dfd2c555a1d94408d981e9bd03781d
Doniben <doniben@esperanto.co>
2020-01-22 19:40:41 +0000

    [FEATURE] script that starts a Flask web application with /, /hbnb, /c/<text>, /number/<n> and number_template

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -3,6 +3,7 @@
 /hbnb, /c/<text>, /python/<text>, /number/<n>/ and /number_template/<n> """
 
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
@@ -24,3 +25,18 @@ def changed_text(text):
 
 @app.route('/python/')
 @app.route('/python/<text>')
+def python_route(text="is cool"):
+    return "Python " + text.replace("_", " ")
+
+
+@app.route('/number/<int:n>')
+def number(n):
+    return ("%d is a number" % n)
+
+
+app.route('number_template/<int:n>')
+def template(n):
+    return render_template('5-number.html', n=n)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
