9beaa8a0630380daa85a45fb7cde2a65be991fbd
Doniben <doniben@esperanto.co>
2020-01-22 03:03:07 +0000

    [FEATURE] script that starts a Flask web application with /, /hbnb, /c/<text>, /number/<n>

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -31,7 +31,7 @@ def python_func(text="is cool"):
 @app.route('/number/')
 @app.route('/number/<int:n>')
 def number(n):
-    return "{:d} is a number".format(n)
+    return ("%d is a number" % n)
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0")
