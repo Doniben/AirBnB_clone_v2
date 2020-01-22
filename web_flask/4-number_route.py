3888b50b611d3af18f559fa0d6adee45122c6e3b
Doniben <doniben@esperanto.co>
2020-01-22 02:56:31 +0000

    [FEATURE] script that starts a Flask web application with /, /hbnb, /c/<text>, /number/<n>

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -6,16 +6,32 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
     return "Hello HBNB"
+
 
 @app.route('/hbnb')
 def hbnb():
     return "HBNB"
 
+
 @app.route('/c/<text>')
 def text_function(text):
     return "C" + text.replace("_", " ")
 
-@
+
+@app.route('/python/')
+@app.route('/python/<text>')
+def python_func(text="is cool"):
+    return "Python " + text.replace("_", " ")
+
+
+@app.route('/number/')
+@app.route('/number/<int:n>')
+def number(n):
+    return "{} is a number".format(n)
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0")
