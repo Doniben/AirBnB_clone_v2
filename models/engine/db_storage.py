80a9dcd17774b5516afde20ed068b6c403292f8b
Doniben <doniben@esperanto.co>
2020-01-23 02:45:34 +0000

    [FEATURE] adding close function calling remove method

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -77,3 +77,7 @@ class DBStorage:
         session = scoped_session(sessionmaker(bind=self.__engine,
                                               expire_on_commit=False))
         self.__session = session()
+
+    def close(self):
+        """ calls remove """
+        self.__session.remove()
